1011f9b19cb613b89599053e20b991febf91f999
Pasha <zzzkelrath@yandex.ru>
2024-12-04 21:15:00 +0700

    Moved from public to private access on remove and insert functions in LRUCache class

--- lru_cache.py
+++ lru_cache.py
@@ -11,30 +11,30 @@ class LRUCache:
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
     
-    def remove(self, node: Node) -> None:
+    def __remove(self, node: Node) -> None:
         prev, next = node.prev, node.next
         prev.next, next.prev = next, prev
     
-    def insert(self, node: Node) -> None:
+    def __insert(self, node: Node) -> None:
         prev, next = self.right.prev, self.right
         prev.next = next.prev = node
         node.next, node.prev = next, prev
     
     def put(self, key, value) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
+            self.__remove(self.cache[key])
         self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+        self.__insert(self.cache[key])
         
         if len(self.cache) > self.capacity:
             lru = self.left.next
-            self.remove(lru)
+            self.__remove(lru)
             del self.cache[lru.key] 
     def get(self, key):
         if key in self.cache:
             node = self.cache[key]
-            self.remove(node)
-            self.insert(node)
+            self.__remove(node)
+            self.__insert(node)
             return node.val
         return None
 
